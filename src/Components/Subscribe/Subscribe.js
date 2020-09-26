747a185a79feface6cc30dca30d22615c3a82c2a
Saurabh pathak <liocatsp@gmail.com>
2020-09-26 13:43:15 +0530

    little fix

--- src/Components/Subscribe/Subscribe.js
+++ src/Components/Subscribe/Subscribe.js
@@ -93,12 +93,12 @@ const Subscribe = () => {
         <Container maxWidth="xl" className={classes.sectionContent} id="How to Subscribe">
             <Grid container alignItems="center" justify="center">
                 <Grid container item alignItems="center" justify="center" lg={11} xl={11}>
-                    <Grid item lg={12} xl={11} >
+                    <Grid item lg={12} xl={11} id="Subscribe">
                         <Typography variant="h4" className={classes.SectionTitle} align="center">
                             How to Subscribe
                         </Typography>
                     </Grid>
-                    <Grid item container justify="center" direction="row" id="Subscribe">
+                    <Grid item container justify="center" direction="row" >
                         <Grid container item justify="center" xs={12} sm={6} md={4} lg={4} xl={4} className={classes.sideGutters}>
                             <Grid>
                                 <div className={classes.stepNumber}>1</div>
